df4c485f321863a665cd16299e4345ec52375797
sharkymid <gastonmatiascamargo@gmail.com>
2021-05-17 20:04:01 -0300

    Implementacion de ContactFactory

---
+++ src/main/java/services/ContactFactory.java
@@ -0,0 +1,10 @@
+package main.java.services;
+
+import main.java.interfaces.IContactService;
+
+public class ContactFactory {
+    public static IContactService buildContactService(String filename){
+        return new ContactService(filename);
+    }
+}
+
